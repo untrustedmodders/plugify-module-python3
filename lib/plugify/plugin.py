4d086f43826b9c9e3e24629bdc83e9e875226b94
Maxim Telezhenko <zlo911111@gmail.com>
2024-06-30 12:44:31 +0300

    Replace tabs to spaces

--- lib/plugify/plugin.py
+++ lib/plugify/plugin.py
@@ -1,7 +1,8 @@
 class Plugin:
-	pass
+    pass
+
 
 class PluginInfo:
-	def __init__(self, class_name):
-		self.class_name = class_name;
-		self.instance = None
+    def __init__(self, class_name):
+        self.class_name = class_name
+        self.instance = None
